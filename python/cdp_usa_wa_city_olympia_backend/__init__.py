0659d764f776523d55368a67ebac2772b9ddb4c3
gregoryfoster <greg@foojutsu.com>
2023-11-19 16:53:50 -0800

    Initial commit

---
+++ python/cdp_usa_wa_city_olympia_backend/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Top-level package for Olympia (Washington, USA)
+CDP Instance backend.
+"""
+
+__author__ = "Cannabis Observer"
+__version__ = "1.0.0"
+
+
+def get_module_version() -> str:
+    return __version__
---
+++ web/src/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-usa-wa-city-olympia-kkughv",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "Olympia (Washington, USA)",
+        timeZone: "America/Los_Angeles",
+        footerLinksSections: [],
+    },
+    features: {
+        // enableClipping: true,
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
